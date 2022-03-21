2f66c99b64a4d9a21bf7628ed47d2c44339071fe
Prince Pandey <princepandey2784@gmail.com>
2022-03-21 10:07:06 +0530

    superior approach

--- swap_alternate.cpp
+++ swap_alternate.cpp
@@ -1,5 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+void swap1(int arr[],int n)
+{
+    for(int i=0;i<n;i=i+2)
+    {
+        
+        if((i+1)<n)
+        {
+             arr[i]=(arr[i] ^ arr[i+1]);
+            arr[i+1]=(arr[i] ^ arr[i+1]);
+            arr[i]=(arr[i] ^ arr[i+1]);
+        }
+    }
+}
 int main()
 {
     int n;
@@ -8,7 +21,7 @@ int main()
     for(int i=0;i<n;i++)
     cin>>arr[i];
     //swap alternate nos.
-    if((n&1)==1)
+  /*  if((n&1)==1)
     {
         //odd
         for(int i=0;i<n-1;i=i+2)
@@ -26,7 +39,8 @@ int main()
             arr[i+1]=(arr[i] ^ arr[i+1]);
             arr[i]=(arr[i] ^ arr[i+1]);
         }
-    }
+    } */
+    swap1(arr,n);
     for(auto x:arr)
     cout<<x<<" ";
     return 0;
